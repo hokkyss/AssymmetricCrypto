6d0db39b0de3da32e29885cd14329c1e5b1168d6
hokkyss <hokkyss2@gmail.com>
2021-11-01 00:12:48 +0700

    onchange and ecc encrypt/decrypt

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 # app.py
 
 import os
+from typing import Dict, Union
 
 from utils.main import proceed
 from utils.utils import PrimeGenerator
-from flask import Flask, render_template, request
+from flask import Flask, json, render_template, request
 
 DEV = os.getenv("FLASK_ENV", "development")
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def encryption():
+    return render_template("home.html")
+
+@app.route("/count", methods=["POST"])
+def count():
+    result: Dict[str, Union[str, int, None]] = {}
     if request.method == "POST":
-        public_key = request.form.get("public-key")
-        private_key = request.form.get("private-key")
-        choice = todo = request.form.get("choice")
-        mode = request.form.get("mode")
-        input_box = request.form.get("input-box")
+        public_key = request.json.get("public-key")
+        private_key = request.json.get("private-key")
+        choice = request.json.get("choice")
+        mode = request.json.get("mode")
+        input_box = request.json.get("input-box")
         result_box = proceed(public_key, private_key, choice, mode, input_box)
     else:
         result_box = ""
-    print("Result box", result_box)
-    return render_template("home.html", result_box = result_box)
 
+    if not result_box: result_box = ""
+
+    return result_box
 
 if __name__ == '__main__':
     app.run(debug=DEV)
--- utils/EllipticCurve.py
+++ utils/EllipticCurve.py
@@ -1,8 +1,9 @@
 from typing import ClassVar, List, Tuple
 from typing_extensions import Final, final
+from random import randint
 
 from .utils import inverse_modulo, pow_mod
-
+ 
 @final
 class EllipticCurve:
     __INFINITY: Final[ClassVar[Tuple[int, int]]] = (0, 10000000000000000000000000000000000000000000000000000000000000000)
@@ -47,6 +48,10 @@ class EllipticCurve:
         return (xR, yR)
     
     @staticmethod
+    def subtract(P: Tuple[int, int], Q: Tuple[int, int], p: int) -> Tuple[int, int]:
+        return EllipticCurve.add(P, EllipticCurve.multiply(Q, -1, p), p)
+    
+    @staticmethod
     def generate(a: int, b: int, p: int) -> List[Tuple[int, int]]:
         """
         Generate all points (x, y) on Elliptic Curve satisfying
@@ -61,3 +66,37 @@ class EllipticCurve:
             modulo_table.append((pow_mod(i, 3, p) + ((a * i) % p) + b) % p)
         
         pass
+
+    @staticmethod
+    def encode(m: int, p: int) -> Tuple[int, int]:
+        return (1, p)
+    
+    @staticmethod
+    def decode(P: Tuple[int, int], p: int) -> int:
+        return p
+
+def encrypt(m: List[int], B: Tuple[int, int], p: int, public_key: Tuple[int, int]) -> List[Tuple[int, int]]:
+    """
+    - `m` list of messages as integer each
+    - `B` is the base Elliptic Curve Point
+    - `p` is any prime number
+    - `public_key` is Pb
+    """
+    result: List[Tuple[Tuple[int, int], Tuple[int, int]]] = []
+
+    for block in m:
+        k = randint(1, p - 1)
+
+        Pm = EllipticCurve.encode(block)
+        Pc = (EllipticCurve.multiply(B, k, p), EllipticCurve.add(Pm, EllipticCurve.multiply(public_key, k, p), p))
+        result.append(Pc)
+
+def decrypt(m: List[Tuple[Tuple[int, int], Tuple[int, int]]], private_key: int, p: int) -> List[int]:
+    result: List[int] = []
+
+    for (a, b) in m:
+        subtractor = EllipticCurve.multiply(a, private_key, p)
+        Pm = EllipticCurve.subtract(b, subtractor, p)
+
+        result.append(EllipticCurve.decode(Pm, p))
+    return result
