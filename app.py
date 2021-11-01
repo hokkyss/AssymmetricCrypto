7c31f138b2ca3e17eb710aac42184a1257cfed73
Michael Hans <michaelhans777@gmail.com>
2021-11-02 00:26:59 +0700

    Add How to Use

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ from flask import Flask, json, render_template, request
 DEV = os.getenv("FLASK_ENV", "development")
 app = Flask(__name__)
 
-@app.route("/upload_public_key", methods=["POST"])
+@app.route("/upload_public_key", methods=['POST'])
 def upload_public_key():
     filename = request.json.get("public-key-file")
     return readFile(filename)
 
-@app.route("/upload_private_key", methods=["POST"])
+@app.route("/upload_private_key", methods=['POST'])
 def upload_private_key():
     filename = request.json.get("private-key-file")
     return readFile(filename)
 
-@app.route("/generation", methods=["GET", "POST"])
+@app.route("/generation", methods=["GET", 'POST'])
 def generation():
     notification = ""
-    if (request.method == "POST"):
+    if (request.method == 'POST'):
         choice = request.json.get("choice")
         notification = generateKey(choice)
         return notification
@@ -34,7 +34,7 @@ def generation():
 def home():
     return render_template("home.html")
 
-@app.route("/execute", methods=["POST"])
+@app.route("/execute", methods=['POST'])
 def execute():
     public_key = request.json.get("public-key")
     private_key = request.json.get("private-key")
@@ -43,6 +43,10 @@ def execute():
     input_box = request.json.get("input-box")
     result_box = proceed(public_key, private_key, choice, mode, input_box)
     return result_box
+
+@app.route("/guideline", methods=['GET'])
+def guideline():
+    return render_template("guideline.html")
 
 if __name__ == '__main__':
     app.run(debug=DEV)
